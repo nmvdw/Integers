2f3afe9da3275ca738661aff89802067ecb8d692
Koen Timmermans <ktbbb5@gmail.com>
2020-09-02 13:37:49 +0200

    Added Add2Cell for prebicategories

--- Bicategories/PathConstructor.v
+++ Bicategories/PathConstructor.v
@@ -5,6 +5,7 @@ Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
+(*Require Import UniMath.CategoryTheory.DisplayedCats.Core.*)
 Require Import UniMath.Bicategories.Core.Bicat. Import Bicat.Notations.
 Require Import UniMath.Bicategories.Core.Examples.OneTypes.
 Require Import UniMath.Bicategories.DisplayedBicats.DispBicat.
@@ -50,25 +51,30 @@ Proof.
 Qed.
 
 
-    
+
   
 (* the objects over (A, a) are 2-cells. They could also be equalities *)
-Definition add_path_endpoints_ob_mor
-           {C : prebicat}
-           {D : disp_prebicat C}
-           {S : pseudofunctor C C}
-           (l r : pseudotrans (comp_pseudofunctor S (pr1_pseudofunctor D)) (pr1_pseudofunctor D))
-           : disp_prebicat_1_id_comp_cells (total_prebicat D).
-Proof.
-  use tpair.
-  - use tpair.
-    + use tpair.
-      * exact (λ Aa, l Aa ==> r Aa).
-      * intros Aa Bb α β ff.
-        exact ((α ▹ #(pr1_pseudofunctor D) ff) • pseudonaturality_of r ff
-               = (pseudonaturality_of l ff) • (#S (#(pr1_pseudofunctor D) ff) ◃ β)).
-    + use tpair.
-      * intros Aa α. simpl. cbn in α.
+Section PathConstructor.
+  Context {C : prebicat}.
+  Variable (D : disp_prebicat C).
+
+  Variable (S : pseudofunctor C C).
+  Variable (l r : pseudotrans (comp_pseudofunctor S (pr1_pseudofunctor D)) (pr1_pseudofunctor D)).
+
+  Definition add_path_endpoints_ob_mor : Core.disp_cat_ob_mor (total_prebicat D).
+  Proof.
+    use tpair.
+    - exact (λ Aa, l Aa ==> r Aa).
+    - intros Aa Bb α β ff.
+      exact ((α ▹ #(pr1_pseudofunctor D) ff) • pseudonaturality_of r ff
+             = (pseudonaturality_of l ff) • (#S (#(pr1_pseudofunctor D) ff) ◃ β)).
+  Defined.      
+
+  Definition add_path_endpoints_id_comp
+    : Core.disp_cat_id_comp (total_prebicat D) add_path_endpoints_ob_mor.
+  Proof.
+    split.
+    * intros Aa α. simpl. cbn in α.
         refine (_ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @_ @ !(_ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @_)).
         -- apply maponpaths.
            apply (pseudotrans_id_alt r Aa).
@@ -133,6 +139,68 @@ Proof.
         -- apply maponpaths.
            apply (!vassocr _ _ _ ).
       * intros Aa Bb Cc ff gg. cbn. intros α β γ ε θ.
-        (* dat hele grote schema *)
-           
-          
+        pose (pseudotrans_comp_alt r ff gg) as pr.
+        pose (pseudotrans_comp_alt l ff gg) as pl.
+        cbn in pr, pl; rewrite pr, pl; clear pr pl.
+        rewrite !vassocr.
+        rewrite vcomp_whisker.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite vcomp_whisker.
+        rewrite !vassocr.
+        apply maponpaths_2.
+        rewrite <- rwhisker_rwhisker.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite <- lwhisker_lwhisker.
+        rewrite !vassocr.
+        apply maponpaths_2.
+        rewrite rwhisker_vcomp.
+        etrans.
+        {
+          apply maponpaths_2.
+          apply maponpaths_2.
+          apply maponpaths.
+          apply ε.
+        }
+        rewrite <- rwhisker_vcomp.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite !vassocr.
+        rewrite <- rwhisker_lwhisker_rassociator.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite lwhisker_vcomp.
+        etrans.
+        {
+          apply maponpaths.
+          apply θ.
+        }
+        apply (!lwhisker_vcomp _ _ _).
+  Qed.
+  
+  Definition add_path_endpoints_1_id_comp_cells : disp_prebicat_1_id_comp_cells (total_prebicat D).
+  Proof.
+    use tpair.
+    - use tpair.
+      + exact add_path_endpoints_ob_mor.
+      + exact add_path_endpoints_id_comp.
+    - intros Aa Bb f g θ. cbn. intros α δ ε γ.
+      exact unit.
+  Defined.
+
+  Definition add_path_endpoints_ops : disp_prebicat_ops' add_path_endpoints_1_id_comp_cells.
+  Proof.
+    repeat split; exact tt.
+  Qed.
+
+  Definition add_path_endpoints_prebicat : disp_prebicat (total_prebicat D).
+  Proof.
+    use tpair.
+    - exists add_path_endpoints_1_id_comp_cells.
+      exact add_path_endpoints_ops.
+    - abstract (repeat split; repeat intro; apply isapropunit).
+  Defined.
+
+  Definition add_path_endpoints : prebicat := total_prebicat add_path_endpoints_prebicat.
+End PathConstructor.
