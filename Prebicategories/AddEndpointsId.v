41532abb244d5ea47ae177436272f2d1bc377d46
Koen Timmermans <ktbbb5@gmail.com>
2021-04-07 10:35:24 +0200

    Identity pseudofunctor

---
+++ Prebicategories/AddEndpointsId.v
@@ -0,0 +1,200 @@
+(* 
+ - Definition of 'DFcell'
+From 'UniMath/Bicategories/DisplayedBicats/Examples/Add2Cell.v'
+ *)
+
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.All.
+Require Import UniMath.CategoryTheory.Core.Categories.
+Require Import UniMath.CategoryTheory.Core.Functors.
+Require Import UniMath.Bicategories.Core.Bicat. Import Bicat.Notations.
+Require Import UniMath.Bicategories.DisplayedBicats.DispBicat.
+
+Require Import Integers.Prebicategories.DispPrebicat.
+Require Import Integers.Prebicategories.PseudoFunctor.
+Import PseudoFunctor.Notations.
+Require Import Integers.Prebicategories.PseudoTransformation.
+Require Import Integers.Prebicategories.Composition.
+Require Import Integers.Prebicategories.Projection.
+
+Local Open Scope cat.
+Local Open Scope mor_disp_scope.
+Local Open Scope bicategory_scope.
+
+(** Auxiliary lemmas **)
+Lemma linvunitor_vcomp {C : prebicat} {a b : C} (f g : a --> b) (θ : f ==> g)
+  : linvunitor f • (identity _ ◃ θ) = θ • linvunitor g.
+Proof.
+  refine (_ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @ _).
+  - apply maponpaths.
+    apply (!id2_right (identity a ◃ θ)).
+  - apply vassocr.
+  - apply maponpaths.
+    apply (!lunitor_linvunitor _).
+  - apply vassocr.
+  - apply maponpaths_2.    
+    apply (!vassocr _ _ _).
+  - apply maponpaths_2.
+    apply maponpaths.
+    apply vcomp_lunitor.
+  - apply maponpaths_2.
+    apply vassocr.
+  - apply maponpaths_2.
+    apply maponpaths_2.
+    apply linvunitor_lunitor.
+  - apply maponpaths_2.
+    apply id2_left.
+Qed.
+
+(** Definition of DFcell **)
+Section AddEndpoints.
+  Context {C : prebicat}.
+  Variable (D : disp_prebicat C).
+
+  Variable (S : pseudofunctor C C).
+  Variable (l r : pseudotrans (comp_pseudofunctor S (pr1_pseudofunctor D)) (pr1_pseudofunctor D)).
+
+  Definition add_path_endpoints_ob_mor : Core.disp_cat_ob_mor (total_prebicat D).
+  Proof.
+    use tpair.
+    - exact (λ Aa, l Aa ==> r Aa).
+    - intros Aa Bb α β ff.
+      exact ((α ▹ #(pr1_pseudofunctor D) ff) • pseudonaturality_of r ff
+             = (pseudonaturality_of l ff) • (#S (#(pr1_pseudofunctor D) ff) ◃ β)).
+  Defined.      
+
+  Definition add_path_endpoints_id_comp
+    : Core.disp_cat_id_comp (total_prebicat D) add_path_endpoints_ob_mor.
+  Proof.
+    split.
+    * intros Aa α. simpl. cbn in α.
+        refine (_ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @_ @ !(_ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @ _ @_)).
+        -- apply maponpaths.
+           apply (pseudotrans_id_alt r Aa).
+        -- cbn.
+           apply maponpaths.
+           apply maponpaths.
+           apply maponpaths.
+           apply maponpaths.
+           apply pseudofunctor_id2.
+        -- apply maponpaths.
+           apply maponpaths.
+           apply maponpaths.
+           apply id2_right.
+        -- apply maponpaths.
+           apply maponpaths_2.
+           apply maponpaths_2.
+           apply maponpaths_2.
+           apply lwhisker_id2.
+        -- apply maponpaths.
+           apply maponpaths_2.
+           apply maponpaths_2.
+           apply id2_left.
+        -- apply vassocr.
+        -- apply maponpaths_2.
+           apply vassocr.
+        -- apply maponpaths_2.
+           apply maponpaths_2.
+           apply vcomp_runitor.
+        -- apply vassocl.
+        -- apply vassocl.
+           
+        -- apply maponpaths_2.
+           apply (pseudotrans_id_alt l Aa).
+        -- cbn.
+           apply maponpaths_2.
+           apply maponpaths.        
+           apply maponpaths.
+           apply maponpaths.
+           apply pseudofunctor_id2.
+        -- apply maponpaths_2.
+           apply maponpaths.
+           apply maponpaths.
+           apply id2_right.
+        -- apply maponpaths_2.
+           apply maponpaths_2.
+           apply maponpaths_2.
+           apply maponpaths_2.
+           apply lwhisker_id2.
+        -- apply maponpaths_2.
+           apply maponpaths_2.
+           apply maponpaths_2.
+           apply id2_left.
+        -- apply vassocl.
+        -- apply maponpaths.
+           apply vcomp_whisker.
+        -- apply vassocl.
+        -- apply maponpaths.
+           apply vassocr.
+        -- apply maponpaths.
+           apply maponpaths_2.
+           apply linvunitor_vcomp.
+        -- apply maponpaths.
+           apply (!vassocr _ _ _ ).
+      * intros Aa Bb Cc ff gg. cbn. intros α β γ ε θ.
+        pose (pseudotrans_comp_alt r ff gg) as pr.
+        pose (pseudotrans_comp_alt l ff gg) as pl.
+        cbn in pr, pl; rewrite pr, pl; clear pr pl.
+        rewrite !vassocr.
+        rewrite vcomp_whisker.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite vcomp_whisker.
+        rewrite !vassocr.
+        apply maponpaths_2.
+        rewrite <- rwhisker_rwhisker.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite <- lwhisker_lwhisker.
+        rewrite !vassocr.
+        apply maponpaths_2.
+        rewrite rwhisker_vcomp.
+        etrans.
+        {
+          apply maponpaths_2.
+          apply maponpaths_2.
+          apply maponpaths.
+          apply ε.
+        }
+        rewrite <- rwhisker_vcomp.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite !vassocr.
+        rewrite <- rwhisker_lwhisker_rassociator.
+        rewrite !vassocl.
+        apply maponpaths.
+        rewrite lwhisker_vcomp.
+        etrans.
+        {
+          apply maponpaths.
+          apply θ.
+        }
+        apply (!lwhisker_vcomp _ _ _).
+  Qed.
+  
+  Definition add_path_endpoints_1_id_comp_cells : disp_prebicat_1_id_comp_cells (total_prebicat D).
+  Proof.
+    use tpair.
+    - use tpair.
+      + exact add_path_endpoints_ob_mor.
+      + exact add_path_endpoints_id_comp.
+    - intros Aa Bb f g θ. cbn. intros α δ ε γ.
+      exact unit.
+  Defined.
+
+  Definition add_path_endpoints_ops : disp_prebicat_ops' add_path_endpoints_1_id_comp_cells.
+  Proof.
+    repeat split; exact tt.
+  Qed.
+
+  (* DFcell *)
+  Definition add_path_endpoints_prebicat : disp_prebicat (total_prebicat D).
+  Proof.
+    use tpair.
+    - exists add_path_endpoints_1_id_comp_cells.
+      exact add_path_endpoints_ops.
+    - abstract (repeat split; repeat intro; apply isapropunit).
+  Defined.
+
+  Definition add_path_endpoints : prebicat := total_prebicat add_path_endpoints_prebicat.
+End AddEndpoints.
---
+++ Prebicategories/Algebra2.v
@@ -0,0 +1,81 @@
+(*
+ - Definition of the displayed prebicategory of F-algebras (DFalg)
+ - The type of 2-cells is `unit`
+From 'UniMath/Bicategories/DisplayedBicats/Examples/Algebras.v' *)
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.All.
+Require Import UniMath.CategoryTheory.Core.Categories.
+Require Import UniMath.CategoryTheory.Core.Functors.
+Require Import UniMath.CategoryTheory.DisplayedCats.Core.
+Require Import UniMath.Bicategories.Core.Bicat. Import Bicat.Notations.
+Require Import UniMath.Bicategories.DisplayedBicats.DispBicat.
+
+Require Import Integers.Prebicategories.Invertible_2cells.
+Require Import Integers.Prebicategories.PseudoFunctor.
+Import PseudoFunctor.Notations.
+Require Import Integers.Prebicategories.DispPrebicat.
+Import DispPrebicat.Notations.
+
+Local Open Scope cat.
+Section Algebra.
+  Context {C : prebicat}.
+  Variable (F : pseudofunctor C C).
+
+  Locate disp_functor_data.
+  Definition disp_alg_ob_mor : Core.disp_cat_ob_mor C.
+  Proof.
+    use tpair.
+    - intro a.
+      exact (F a --> a).
+    - intros a b ha hb f.
+      exact (invertible_2cell (ha · f) (#F f · hb)).
+  Defined.
+
+  Definition disp_alg_id_comp
+    : Core.disp_cat_id_comp C disp_alg_ob_mor.
+  Proof.
+    split.
+    - cbn. intros a ha.
+      refine (runitor ha • linvunitor ha • (pseudofunctor_id F a ▹ ha) ,, _).
+      is_iso.
+      exact (pseudofunctor_id F a).
+    - cbn. intros a b c f g ha hb hc ff gg.
+      refine ((lassociator ha f g)
+                • (ff ▹ g)
+                • rassociator (#F f) hb g
+                • (#F f ◃ gg)
+                • lassociator (#F f) (#F g) hc
+                • (pseudofunctor_comp F f g ▹ hc) ,, _).
+      is_iso.
+      + exact ff.
+      + exact gg.
+      + exact (pseudofunctor_comp F f g).
+  Defined.
+
+  Definition disp_alg_1_id_comp_cells : disp_prebicat_1_id_comp_cells C.
+  Proof.
+    use tpair.
+    - use tpair.
+      + exact disp_alg_ob_mor.
+      + exact disp_alg_id_comp.
+    - intros a b f g θ. cbn. intros ha hb ff gg.
+      exact unit.
+  Defined.
+
+  Definition disp_alg_ops : disp_prebicat_ops' disp_alg_1_id_comp_cells.
+  Proof.
+    repeat split.
+  Qed.
+
+  Definition disp_alg_ops_laws : disp_prebicat_laws (_ ,, disp_alg_ops).
+  Proof.
+    repeat split; intro; intros; apply isapropunit.
+  Qed.
+
+  (* DFalg *)
+  Definition disp_alg_prebicat : disp_prebicat C
+    := (_ ,, disp_alg_ops_laws).
+
+  Definition prebicat_algebra := total_prebicat disp_alg_prebicat.
+End Algebra.
+
